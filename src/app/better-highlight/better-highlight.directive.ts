4921c5acdd5396422d1c13a37423968907e6130b
dabc4694 <darshan.shah130@gmail.com>
2017-07-23 19:09:10 -0500

    Better Hightlight directive added

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+}
